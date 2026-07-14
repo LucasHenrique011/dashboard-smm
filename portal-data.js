6623b3a2c1eb42d66d655d58b955fd21b667fc72
LucasHenrique011 <lucascandido07072007@icloud.com>
2026-07-13 23:50:56 -0300

    Atualiza portal do cliente

--- portal-data.js
+++ portal-data.js
@@ -1006,5 +1006,5 @@ const PORTAL_DATA = {
     "lucas": "lucas2026ls",
     "pedroblackeguil": "pedr2026ls"
   },
-  "updatedAt": "2026-07-14T02:48:41.111Z"
+  "updatedAt": "2026-07-14T02:50:55.120Z"
 };
