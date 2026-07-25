fdeb0455354408955c135ade185abcabb856365f
LucasHenrique011 <lucascandido07072007@icloud.com>
2026-07-25 08:02:15 -0300

    Atualiza portal do cliente

--- portal-data.js
+++ portal-data.js
@@ -811,7 +811,7 @@ const PORTAL_DATA = {
       "title": "ABRINDO CAIXA",
       "pillar": "DESEJO",
       "date": "2026-07-21",
-      "status": "editado",
+      "status": "postado",
       "ref": "https://www.tiktok.com/@onestopsocials/video/7558334893122440466?is_from_webapp=1&sender_device=pc&web_id=7655682300580496904",
       "roteiro": ""
     },
@@ -1087,5 +1087,5 @@ const PORTAL_DATA = {
     "lucas": "lucas2026ls",
     "pedroblackeguil": "pedr2026ls"
   },
-  "updatedAt": "2026-07-25T11:01:59.845Z"
+  "updatedAt": "2026-07-25T11:02:13.879Z"
 };
